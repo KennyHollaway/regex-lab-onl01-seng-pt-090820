f6140f2895e2246b9fbf2780c41bf6b6b1fdbce4
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-22 23:10:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  array = 
-end
+  array = text.split(" ")
+  
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
